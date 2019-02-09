aa4a386a98302399053c2cc30a333c693ac63ecd
edi-garsell46 <edi.nasa@gmail.com>
2019-02-09 15:56:04 +0700

    first commit

---
+++ xlpy/__init__.py
@@ -0,0 +1,10 @@
+from .config import Config
+from .client import XL
+
+__copyright__       = 'Copyright 2018 by Edi Garsell'
+__version__         = '5.0.0'
+__author__          = 'Edi Garsell'
+__author_email__    = 'edi.garsell@gmail.com'
+__url__             = 'https://github.com/edi-garsell46/tembaktembakan.git'
+
+__all__ = ['Config','XL']
